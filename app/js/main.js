1d714646dd78650cf802d3cdd46217ad3debbbcf
schandmaulchen <schandmaulchenn@gmail.com>
2021-06-04 00:05:27 +0300

    fix task 14

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,26 @@
-$(function() {
-
-  $('.menu-btn, .menu__link').on('click', function(){
+$(function() {   
+  
+  $('.menu-btn').on('click', function(){
     $('.header__menu').toggleClass('header__menu--active');
     $('.user-nav').toggleClass('user-nav--open');
     $('body').toggleClass('lock-scroll');
     $('.menu-btn').toggleClass('menu-btn--close');
   });
+
+  $('.menu__link').on('click', function(){
+    $('.menu-btn').removeClass('menu-btn--close');
+    $('.user-nav').removeClass('user-nav--open');
+    $('.header__menu').removeClass('header__menu--active');
+  });
+
+  // $('body').on('click', function(e){
+  //   if(e.target !== $('.header__menu') 
+  //     && e.target !== $('.menu-btn') 
+  //     && $('.header__menu').hasClass('header__menu--active')) {
+  //       console.log(e.target);
+  //       $('.header__menu').removeClass('header__menu--active');
+  //   }
+  // });
 
   $('.main-slider__inner').slick({
     dots: true,
